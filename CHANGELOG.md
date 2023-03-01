9fa965d9914917e00f8f9aeff1e885173fcb5b2f
Ilia Golubev <java_se@mail.ru>
2023-03-01 18:01:26 +0300

    Global FIX

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,17 +1,5 @@
 # Changelog
 
-## v3.0.0 - April 9, 2017
+## v3.0.2 - March 1, 2023
 
-- Position drag handles relative to editor, not document
-- Simplify toolbar and use floats
-- Improved styling
-
-## v2.0.0 - April 3, 2017
-
-- Added toolbar for alignment
-- Modularized
-- Fix resize bug
-
-## v1.0.0 - March 25, 2017
-
-- Initial version
+- Add element id for overlay div
--- src/ImageResize.js
+++ src/ImageResize.js
@@ -41,8 +41,6 @@ export default class ImageResize {
 
         // respond to clicks inside the editor
         this.quill.root.addEventListener('click', this.handleClick, false);
-		
-		this.quill.root.addEventListener('blur', this.handleFocusOut);
 
         this.quill.root.parentNode.style.position = this.quill.root.parentNode.style.position || 'relative';
 
@@ -86,12 +84,6 @@ export default class ImageResize {
 
         this.modules = [];
     };
-	
-	handleFocusOut = (evn) => {
-        if(this.img){
-            this.hideOverlay();
-        }
-    }
 
     handleClick = (evt) => {
         if (evt.target && evt.target.tagName && evt.target.tagName.toUpperCase() === 'IMG') {
@@ -136,6 +128,7 @@ export default class ImageResize {
 
         // Create and add the overlay
         this.overlay = document.createElement('div');
+        this.overlay.setAttribute("id", "quill-resize-overlay");
         Object.assign(this.overlay.style, this.options.overlayStyles);
 
         this.quill.root.parentNode.appendChild(this.overlay);
